1cb3e0a0c607b8c4877d3ec9b6c84ac160d29aaa
unknown <1223439361@qq.com>
2019-04-02 14:46:52 -0400

    solution

--- GlassFalling.java
+++ GlassFalling.java
@@ -1,26 +1,121 @@
 /**
  * Glass Falling
  */
+// Glass Falling uses recursion, memorization, and buttom-up
 public class GlassFalling {
 
   // Do not change the parameters!
   public int glassFallingRecur(int floors, int sheets) {
-    // Fill in here and change the return
-    return 0;
+	//base case 
+	 
+      if(floors==0 || floors==1)
+
+          return floors;
+
+      if(sheets==1)
+
+          return floors;
+
+
+
+      int min= Integer.MAX_VALUE, temp;
+
+      //two cases  a) egg  breaks b) egg will not break
+
+      for (int i = 1; i <=floors ; i++) {
+
+          temp = Math.max(glassFallingRecur(sheets-1,i-1), glassFallingRecur(sheets, floors-i));
+
+          min = Math.min(temp,min);
+
+      }
+
+      return min + 1;
+
+  
+   
   }
 
   // Optional:
   // Pick whatever parameters you want to, just make sure to return an int.
-  public int glassFallingMemoized() {
-    // Fill in here and change the return
-    return 0;
+   public int glassFallingMemoized(int sheets, int floors) {
+	    int[][] memo = new int[sheets + 1][floors];
+	    for (int x = 0; x <= sheets; x++) {
+	      for (int y = 0; y < floors; y++) {
+	        memo[x][y] = Integer.MAX_VALUE;
+	      }
+	    }
+	    return glassFallingMemoized(sheets, floors, memo);
+	  }
+  public int glassFallingMemoized(int sheets, int floors, int[][]memo ) {
+	     if (1>=sheets) {
+	    	 return floors;
+  }
+    if (1>=floors) {
+    	return 1;
+    }
+    
+   int best= floors +1 ;
+   for(int a,b, x=1; x< floors; x++ ) {
+	   if ((a = memo[sheets-1][floors-1])== Integer.MAX_VALUE ) {
+		  a = glassFallingMemoized( sheets-1, floors-1, memo);
+	   
+	   }
+	   if ((b=memo[sheets][floors -x]) == Integer.MAX_VALUE) {
+		   b= glassFallingMemoized ( sheets, floors-x, memo);
+		   
+	   }
+	   best= Math.min(best, Math.max(a, b));
+	   
+   }
+    	if( floors < memo[0].length) {
+    		memo[sheets][floors]=best+1;
+    	}
+	    	 
+    
+    return best + 1;
   }
 
   // Do not change the parameters!
+ 
   public int glassFallingBottomUp(int floors, int sheets) {
-    // Fill in here and change the return
-    return 0;
-  }
+	    int[][] glassFloor= new int [sheets+1][floors+1]; 
+	    int res;
+	    int i, j, x; 
+	  
+	    // We need one trial for one floor and 0 trials for 0 floors 
+	    for (i = 1; i <= sheets; i++) 
+	    { 
+	        glassFloor[i][1] = 1; 
+	        glassFloor[i][0] = 0; 
+	    } 
+	  
+	    // We always need j trials for one egg and j floors. 
+	    for (j = 1; j <= floors; j++) 
+	        glassFloor[1][j] = j; 
+	  
+	    // Fill rest of the entries in table using optimal substructure 
+	     
+	    for (i = 2; i <= sheets; i++) 
+	    { 
+	        for (j = 2; j <= floors; j++) 
+	        { 
+	            glassFloor[i][j] =Integer.MAX_VALUE; 
+	            for (x = 1; x <= j; x++) 
+	            { 
+	                res = 1 + Math.max(glassFloor[i-1][x-1], glassFloor[i][j-x]); 
+	                if (res < glassFloor[i][j]) 
+	                    glassFloor[i][j] = res; 
+	            } 
+	        } 
+	    } 
+	  
+	     
+	    return glassFloor[sheets][floors]; 
+	} 
+    
+   
+  
 
 
   public static void main(String args[]){
@@ -36,3 +131,5 @@ public class GlassFalling {
       System.out.println(minTrials2Recur + " " + minTrials2Bottom);
   }
 }
+
+
--- RodCutting.java
+++ RodCutting.java
@@ -1,33 +1,134 @@
+
 /**
+
  * Rod cutting problem described in Chapter 15 of textbook
+
  */
+// rod cutting approach with recurison, memorization, and buttom up
+
 public class RodCutting {
+ // approach with memorize
+	public int Memorize(int[] prices, int n) {
+
+        int[] r = new int[n + 1];
+
+        for (int i = 0; i < r.length; i++) {
+
+            r[i] = -1;
+
+        }
+
+        return MemorizeA(prices, n, r);
+
+    }
+
+
+
+    private int  MemorizeA(int[] prices, int n, int[] r) {
+
+        if (r[n] >= 0) {
+
+            return r[n];
+
+        }
+
+        int max = Integer.MIN_VALUE;
+
+        if (n == 0) {
+
+            max = 0;
+
+        } else {
+
+            for (int i = 0; i < n; i++) {
+
+                max = Math.max(max, prices[i] + MemorizeA(prices, n - i - 1, r));
+
+            }
+
+        }
+
+        r[n] = max;
+
+        return max;
+
+    }
+
+
+
 
   // Do not change the parameters!
+  // approach with recurison
+	
+
   public int rodCuttingRecur(int rodLength, int[] lengthPrices) {
-    return 0;
+	  if(rodLength<=0 )
+		  return 0;
+	  int max_val = Integer.MIN_VALUE ;
+	  for(int i=0; i<rodLength;i++)
+		  max_val= Math.max(max_val, 
+				  lengthPrices[i]+rodCuttingRecur(rodLength-i-1 ,lengthPrices));
+
+    return max_val;
+
   }
 
+
+
   // Do not change the parameters!
+// dynamic programming BottomUp
+
   public int rodCuttingBottomUp(int rodLength, int[] lengthPrices) {
-    return 0;
+	  int[]result= new int[rodLength+1];
+	  result[0]=0;
+	  for(int i= 1; i<= rodLength;i++) {
+		  int max=-1;
+		  for(int j =1 ; j < i; j++) {
+			  max=Math.max(max, lengthPrices[j]+ result[i-(j+1)]);
+			  result[i]= max;
+		  }
+		  
+	  }
+
+    return result[rodLength];
+
   }
+
+
+
 
 
   public static void main(String args[]){
+
       RodCutting rc = new RodCutting();
 
+
+
       // In your turned in copy, do not touch the below lines of code.
+
       // Make sure below is your only output.
+
       int length1 = 7;
+
       int[] prices1 = {1, 4, 7, 3, 19, 5, 12};
+
       int length2 = 14;
+
       int[] prices2 = {2, 5, 1, 6, 11, 15, 17, 12, 13, 9, 10, 22, 18, 26};
+
       int maxSell1Recur = rc.rodCuttingRecur(length1, prices1);
+
       int maxSell1Bottom = rc.rodCuttingBottomUp(length1, prices1);
+
       int maxSell2Recur = rc.rodCuttingRecur(length2, prices2);
+
       int maxSell2Bottom = rc.rodCuttingBottomUp(length2, prices2);
+
       System.out.println(maxSell1Recur + " " + maxSell1Bottom);
+
       System.out.println(maxSell2Recur + " " + maxSell2Bottom);
+
   }
+
 }
+           
